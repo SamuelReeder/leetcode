53baab96f70f12de23e2b5e4e7d4aca218300b2a
Samuel Reeder <samuel.reeder8@gmail.com>
2025-09-11 22:35:04 -0600

    pascal triangle

---
+++ c++/pascalTriangle.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        using namespace std;
+        
+        vector<vector<int>> triangle(1, {1});
+
+        for (int i = 0; i < numRows - 1; i++) {
+
+            int rowSize = triangle[i].size() + 1;
+            vector<int> row(rowSize);
+
+            for (int j = 0; j < rowSize; j++) {
+
+                if (j > 0)
+                    row[j] += triangle[i][j - 1];
+
+                if (j < triangle[i].size())
+                    row[j] += triangle[i][j];
+            }
+
+            triangle.push_back(row);
+        }
+
+        return triangle;
+
+    }
+};
